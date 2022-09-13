93366e7dedc3483755585b23f1e9fd92fa2cd553
JeongWooHyeon <jeong_wh@stoneb.jp>
2022-09-13 18:42:31 +0900

    파일 업로드 하다말음

--- src/main/java/com/example/exam/HomeController/LookBookController.java
+++ src/main/java/com/example/exam/HomeController/LookBookController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.util.List;
 
@@ -25,8 +28,23 @@ public class LookBookController {
 
         model.addAttribute("lookbooks", lookbooks);
 
-
-
         return "/menu/lookbook";
+    }
+
+    @RequestMapping("/lookbookWrite")
+    public String lookbookWrite(Model model) {
+        log.info("lookbookWrite() Get 호출");
+
+
+        return "/menu/lookbookWrite";
+    }
+
+    @RequestMapping(value = "/lookbookWrite", method = RequestMethod.POST)
+    public String lookbookWrite(MultipartHttpServletRequest mhs) {
+        log.info("lookbookWrite() Post 호출");
+
+        MultipartFile file = mhs.getFile("filename");
+
+        return "/menu/lookbookWrite";
     }
 }
